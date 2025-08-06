5f8d106472aa2925f3437adf9b3b3079efb0d24e
GADTM <activacionesgadtm@gmail.com>
2025-08-06 18:13:14 -0300

    Add files via upload

---
+++ netlify/functions/buscarQSL.js
@@ -0,0 +1,45 @@
+
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  const { callSign } = event.queryStringParameters;
+
+  if (!callSign) {
+    return { statusCode: 400, body: "Falta el indicativo" };
+  }
+
+  const folder = `qsl/${callSign}`;
+  const cloudName = "dgv9hbtxd";
+  const apiKey = "535366383571213";
+  const apiSecret = "pO3Rp0IPWC352bf-vvlIw1yua88";
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/image`;
+
+  try {
+    const res = await axios.get(url, {
+      auth: { username: apiKey, password: apiSecret },
+      params: {
+        prefix: folder,
+        max_results: 100
+      }
+    });
+
+    const images = res.data.resources.map(img => ({
+      url: img.secure_url,
+      public_id: img.public_id,
+      format: img.format
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(images)
+    };
+
+  } catch (error) {
+    console.error("Error desde Cloudinary:", error.message);
+    return {
+      statusCode: 500,
+      body: "Error al consultar imágenes"
+    };
+  }
+};
